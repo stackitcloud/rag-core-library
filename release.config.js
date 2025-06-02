d7d754015ab06cf38c52b14cdc3b19aab7bf42e5
Andreas Klos <andreas.klos@stackit.cloud>
2025-06-02 15:02:55 +0200

    feat: enhance semantic release configuration with conventional changelog support

--- release.config.js
+++ release.config.js
@@ -1,8 +1,12 @@
 const config = {
     branches: ['main'],
     plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
+      ['@semantic-release/commit-analyzer', {
+        preset: 'conventionalcommits'
+      }],
+      ['@semantic-release/release-notes-generator', {
+        preset: 'conventionalcommits'
+      }],
       '@semantic-release/git',
       '@semantic-release/github'
     ]
