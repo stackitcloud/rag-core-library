a6209daa3f3c0385998bce30fba49a3e9f250b09
Andreas Klos <andreas.klos@stackit.cloud>
2025-06-03 13:55:39 +0200

    feat: implement SitemapExtractor and SitemapLangchainDocument2InformationPiece classes

---
+++ extractor-api-lib/src/extractor_api_lib/impl/extractors/sitemap_extractor.py
@@ -0,0 +1,62 @@
+"""Module for the DefaultSitemapExtractor class."""
+
+from langchain_community.document_loaders import SitemapLoader
+
+from extractor_api_lib.impl.types.extractor_types import ExtractorTypes
+from extractor_api_lib.models.dataclasses.internal_information_piece import InternalInformationPiece
+from extractor_api_lib.models.extraction_parameters import ExtractionParameters
+from extractor_api_lib.extractors.information_extractor import InformationExtractor
+from extractor_api_lib.impl.mapper.sitemap_document2information_piece import (
+    SitemapLangchainDocument2InformationPiece,
+)
+
+
+class SitemapExtractor(InformationExtractor):
+    """Implementation of the InformationExtractor interface for confluence."""
+
+    def __init__(
+        self,
+        mapper: SitemapLangchainDocument2InformationPiece,
+    ):
+        """
+        Initialize the SitemapExtractor.
+
+        Parameters
+        ----------
+        mapper : SitemapLangchainDocument2InformationPiece
+            An instance of SitemapLangchainDocument2InformationPiece used for mapping langchain documents
+            to information pieces.
+        """
+        self.mapper = mapper
+
+    @property
+    def extractor_type(self) -> ExtractorTypes:
+        return ExtractorTypes.SITEMAP
+
+    async def aextract_content(
+        self,
+        extraction_parameters: ExtractionParameters,
+    ) -> list[InternalInformationPiece]:
+        """
+        Asynchronously extracts information pieces from Sitemap.
+
+        Parameters
+        ----------
+        extraction_parameters : ExtractionParameters
+            The parameters required to connect to and extract data from Sitemap.
+
+        Returns
+        -------
+        list[InternalInformationPiece]
+            A list of information pieces extracted from Sitemap.
+        """
+        # Convert list of key value pairs to dict
+        confluence_loader_parameters = {
+            x.key: int(x.value) if x.value.isdigit() else x.value for x in extraction_parameters.kwargs
+        }
+        # Drop the document_name parameter as it is not used by the SitemapLoader
+        if "document_name" in confluence_loader_parameters:
+            confluence_loader_parameters.pop("document_name", None)
+        document_loader = ConfluenceLoader(**confluence_loader_parameters)
+        documents = document_loader.load()
+        return [self.mapper.map_document2informationpiece(x, extraction_parameters.document_name) for x in documents]
---
+++ extractor-api-lib/src/extractor_api_lib/impl/mapper/sitemap_document2information_piece.py
@@ -0,0 +1,6 @@
+
+
+
+class SitemapLangchainDocument2InformationPiece:
+   def __init__(self):
+       raise NotImplementedError("This method is not implemented.")
