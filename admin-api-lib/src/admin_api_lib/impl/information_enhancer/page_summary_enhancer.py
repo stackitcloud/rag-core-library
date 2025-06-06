14ca90a114986ec341e1a774dd792865467a2499
Andreas Klos <andreas.klos@stackit.cloud>
2025-06-06 12:46:38 +0200

    refactor: remove redundant whitespace and streamline summary creation logic

--- admin-api-lib/src/admin_api_lib/impl/information_enhancer/page_summary_enhancer.py
+++ admin-api-lib/src/admin_api_lib/impl/information_enhancer/page_summary_enhancer.py
@@ -27,7 +27,6 @@ class PageSummaryEnhancer(SummaryEnhancer):
     BASE64_IMAGE_KEY = "base64_image"
     DEFAULT_PAGE_NR = 1
 
-
     async def _asummarize_page(self, page_pieces: list[Document], config: Optional[RunnableConfig]) -> Document:
         full_page_content = " ".join([piece.page_content for piece in page_pieces])
         summary = await self._summarizer.ainvoke(full_page_content, config)
@@ -37,7 +36,6 @@ class PageSummaryEnhancer(SummaryEnhancer):
         meta["type"] = ContentType.SUMMARY.value
 
         return Document(metadata=meta, page_content=summary)
-
 
     async def _acreate_summary(self, information: list[Document], config: Optional[RunnableConfig]) -> list[Document]:
         distinct_pages = []
@@ -51,20 +49,13 @@ class PageSummaryEnhancer(SummaryEnhancer):
             for compare_info in information:
                 if compare_info.metadata.get("page", self.DEFAULT_PAGE_NR) == page:
                     group.append(compare_info)
-            if self._chunker_settings and len(" ".join([item.page_content for item in group])) < self._chunker_settings.max_size:
+            if (
+                self._chunker_settings
+                and len(" ".join([item.page_content for item in group])) < self._chunker_settings.max_size
+            ):
                 continue
             grouped.append(group)
 
         summary_tasks = [self._asummarize_page(info_group, config) for info_group in tqdm(grouped)]
 
         return await gather(*summary_tasks)
-
-    async def _asummarize_page(self, page_pieces: list[Document], config: Optional[RunnableConfig]) -> Document:
-        full_page_content = " ".join([piece.page_content for piece in page_pieces])
-        summary = await self._summarizer.ainvoke(full_page_content, config)
-        meta = {key: value for key, value in page_pieces[0].metadata.items() if key != self.BASE64_IMAGE_KEY}
-        meta["id"] = sha256(str.encode(full_page_content)).hexdigest()
-        meta["related"] = meta["related"] + [piece.metadata["id"] for piece in page_pieces]
-        meta["type"] = ContentType.SUMMARY.value
-
-        return Document(metadata=meta, page_content=summary)
